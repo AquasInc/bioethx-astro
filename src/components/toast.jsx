ea5902d3d8b066dd8531c55498aaa5c12d70f820
Sergio <ortiz.sergio0303@gmail.com>
2024-06-10 14:01:09 -0400

    feat(index): remove posts

--- src/components/toast.jsx
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/Toast.module.css';
-
-const Toast = () => (
-    <a href={"/IME2023"} className={styles.toast}>{'\uD83D\uDC4B'} IME 2023</a>
-);
-
-export default Toast;
